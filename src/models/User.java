def951be51db2cc0ee63015a8abcdc0ddccb6931
yasser-aboufaris <azertyasser321@gmail.com>
2025-09-18 16:14:22 +0100

    feat: add the static attribute current user to hold the session of the authentified user

--- src/models/User.java
+++ src/models/User.java
@@ -5,6 +5,8 @@ public class User {
     private String username;
     private String password;
 
+    private static int currentUserId = 0; // 0 = no user logged in
+
     public User() {}
 
     public User(String username, String password) {
@@ -18,6 +20,18 @@ public class User {
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
 
+    public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
-    public String getPassword(){ return password; }
+
+    public static void setCurrentUser(int id) {
+        currentUserId = id;
+    }
+
+    public static int getCurrentUser() {
+        return currentUserId;
+    }
+
+    public static boolean isLoggedIn() {
+        return currentUserId != 0;
+    }
 }
